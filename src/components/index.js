a64ddcc530d40b5243e697cb4681f77395170d37
SUPARNA DHARA <dhara.sup@gmail.com>
2024-03-19 12:21:12 +0530

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,22 @@
+import { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas } from './canvas';
+import Hero from './Hero';
+import Navbar from './Navbar';
+import About from './About';
+import Tech from './Tech';
+import Education from './Education';
+import Projects from './Projects';
+import Contact from './Contact';
+
+export {
+  Hero,
+  Navbar,
+  About,
+  Tech,
+  Education,
+  Projects,
+  Contact,
+  EarthCanvas, 
+  BallCanvas, 
+  ComputersCanvas, 
+  StarsCanvas
+}
