ba1ff284e67bfc8f3db36295564c307c1f6c8d88
SUPARNA DHARA <dhara.sup@gmail.com>
2024-03-19 12:26:14 +0530

    name change

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,7 +14,7 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#915EFF]'>Suparna</span>
+            Hi, I'm <span className='text-[#915EFF]'>Suparna Dhara</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
           B.Tech in Computer Science Graduate <br className='sm:block hidden' />
